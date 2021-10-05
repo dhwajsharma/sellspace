4749e3a822cfa08c8d7f23206d399c0894b92744
Tanishq Maheshwari <67184181+tanishq-m@users.noreply.github.com>
2021-10-05 10:24:42 +0530

    Improved UI
    Improved UI when opening a post

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import Header from './components/Header/Header';
 import {
