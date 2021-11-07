fec7707540217cc0861fdae39ffc5e32b3ea76d2
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-11-07 08:25:32 +0530

    Fixed Styling of openpost and modal

--- src/components/OpenPost/OpenPost.js
+++ src/components/OpenPost/OpenPost.js
@@ -1,45 +1,40 @@
 import { useSelector } from "react-redux";
 import { selectpostIsOpen } from "../../features/postSlice";
-import "./OpenPost.css"
+import "./OpenPost.css";
 
 const OpenPost = () => {
-    const selectedPost = useSelector(selectpostIsOpen);
+  const selectedPost = useSelector(selectpostIsOpen);
 
-    return (
+  return (
+    <div className="openpost">
+      <div className=" col-lg-6  openpost__left">
+        <img src={selectedPost?.postImage} alt="" />
+      </div>
 
-
-        <div className="openpost">
-            <div className=" col-lg-6  openpost__left">
-                <img src={selectedPost?.postImage} alt="" />
-            </div>
-
-            <div className=" col-lg-6 openpost__right">
-                <h2>{selectedPost?.name}</h2>
-                <div className="openpost__subHeading">
-                    <p>{selectedPost?.email}</p>
-                </div>
-                <div className="openpost__message">
-
-                    <h2>Title: </h2>
-                    <p>{selectedPost?.title}</p>
-
-                    <h2>Description: </h2>
-                    <p>{selectedPost?.description}</p>
-
-                    <h2>Price: </h2>
-                    <p>{"₹ "}{selectedPost?.price}</p>
-
-                    <h2>Contact Number: </h2>
-                    <p>{selectedPost?.number}</p>
-                </div>
-            </div>
+      <div className=" col-lg-6 openpost__right">
+        <h2>{selectedPost?.name}</h2>
+        <div className="openpost__subHeading">
+          <p>{selectedPost?.email}</p>
         </div>
+        <div className="openpost__message">
+          <h2>Title: </h2>
+          <p>{selectedPost?.title}</p>
 
-    )
-}
+          <h2>Description: </h2>
+          <p>{selectedPost?.description}</p>
 
-export default OpenPost
+          <h2>Price: </h2>
+          <p>
+            {"₹ "}
+            {selectedPost?.price}
+          </p>
 
+          <h2>Contact Number: </h2>
+          <p>{selectedPost?.number}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-
+export default OpenPost;
