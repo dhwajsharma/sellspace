8e81a0c46e79aee4cb8839a7f160c2cb09507324
dhwajsharma <dhwajsharma10@gmail.com>
2021-11-27 10:06:40 +0530

    Added team links and improved styling

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -2,6 +2,8 @@ import "./Team.css";
 import { Button } from "@mui/material";
 import Dhwaj from "../../assets/Dhwaj.png";
 import Tanishq from "../../assets/Tanishq.jpeg";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Team = () => {
   return (
@@ -12,11 +14,26 @@ const Team = () => {
         </div>
         <div className="team__name">
           <h2>Dhwaj Sharma</h2>
+          <div className="team__title">
+            <div>Front-End Developer</div>
+          </div>
+          <div className="team__links">
+            <a
+              href="https:github.com/dhwajsharma"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <GitHubIcon />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/dhwajsharma/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <LinkedInIcon />
+            </a>
+          </div>
         </div>
-        <div className="team__title">
-          <div>Front-End Developer</div>
-        </div>
-        <div className="team__links"></div>
       </div>
       <div className="team__container">
         <div className="team__image">
@@ -28,7 +45,22 @@ const Team = () => {
         <div className="team__title">
           <div>UI/UX Designer</div>
         </div>
-        <div className="team__links"></div>
+        <div className="team__links">
+          <a
+            href="https://github.com/tanishq-m"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <GitHubIcon />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/tanishq-maheshwari-5605b71ab/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <LinkedInIcon />
+          </a>
+        </div>
       </div>
     </div>
   );
